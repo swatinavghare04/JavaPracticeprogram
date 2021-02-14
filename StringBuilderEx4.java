6b3b3c5c03be80bfa52ba3befb850cf37b6feb9e
swatinavghare04 <75429629+swatinavghare04@users.noreply.github.com>
2021-02-14 13:40:01 +0530

    StringBuilder delete() method

---
+++ StringBuilderEx4.java
@@ -0,0 +1,11 @@
+// StringBuilder insert() method
+
+class StringBuilderEx4
+{
+ public static void main(String[] args)
+ {
+  StringBuilder sb = new StringBuilder("Hello");
+  sb.delete(1,3);
+  System.out.println(sb);//print Hlo
+ }
+}
